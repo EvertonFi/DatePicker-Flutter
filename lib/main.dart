e3425a729aaa6d78e0b31c5ed28baf1172e72413
EvertonFi <jose.everton.f@hotmail.com>
2020-03-26 12:25:28 -0300

    Update TimePicker

--- lib/main.dart
+++ lib/main.dart
@@ -19,25 +19,43 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: IconButton(
-            icon: Icon(Icons.date_range),
-            onPressed: () async {
-              final data = await showDatePicker(
-                context: context,
-                initialDate: DateTime.now(),
-                firstDate: DateTime(2018),
-                lastDate: DateTime(2021),
-                locale: Localizations.localeOf(context),
-              );
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.date_range),
+              onPressed: () async {
+                final data = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(2018),
+                  lastDate: DateTime(2021),
+                  locale: Localizations.localeOf(context),
+                );
 
-              if (data != null) {
-                final datapt =
-                    DateFormat(DateFormat.YEAR_MONTH_DAY, 'pt_Br').format(data);
+                if (data != null) {
+                  final datapt = DateFormat(DateFormat.YEAR_MONTH_DAY, 'pt_Br')
+                      .format(data);
 
-                print(datapt);
-              }
-              
-            }),
+                  print(datapt);
+                }
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.access_time),
+              onPressed: () async {
+                final time = await showTimePicker(
+                  context: context,
+                  initialTime: TimeOfDay.now(),
+                );
+
+                if (time != null) {
+                  print("${time.hour}:${time.minute}");
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
